4087c07de7ce94455643dc00a6138460461baf2a
Tino Reyna <tinoreyna1984@gmail.com>
2022-03-10 23:49:04 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
+/**
+ * App para uso de React Bootstrap
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        React Bootstrap <br/>
+        <Button variant="primary" onClick={() => alert("Holaaaaaa...")}>Hazme clic</Button>{' '}
+      </div>
     </div>
   );
 }
